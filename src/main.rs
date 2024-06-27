f789b8e1bb30697c41f07415a6d137afebbfff66
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-06-27 16:31:45 +0300

    created the poem file and read it

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs;
+// filesystem module
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -6,8 +8,14 @@ fn main() {
     let query = &args[1];
     let file_path = &args[2];
 
-    println!("Searching for {query}");
+    println!("Searching for '{query}'");
     println!("In file {file_path}");
     // gives an error if the program is run without any arguments
     // saves to the right variables
+
+    let contents = fs::read_to_string(file_path).expect("Something went wrong reading the file");
+    // read the file and save it to the contents variable
+    // expect is a method that handles errors
+
+    println!("With text:\n{}", contents);
 }
