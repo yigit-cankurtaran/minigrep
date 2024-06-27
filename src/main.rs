231ff4219307eddbaca42070447fab901d857e53
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-06-27 16:27:54 +0300

    implemented arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {query}");
+    println!("In file {file_path}");
+    // gives an error if the program is run without any arguments
+    // saves to the right variables
 }
